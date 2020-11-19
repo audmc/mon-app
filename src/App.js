6a58bfaf9be9d8296ab1fc9d5e1cad28e3d233c0
Moussaillon <moussaillon@Mac-Code-02.local>
2020-11-19 15:11:18 +0100

    MAJ- index.js-exercice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Kylian',
+  lastName: 'Mbappé'
+};
+
+const moi = {
+  firstName: 'Audrey',
+  lastName: 'Masson'
+};
+
+const element = (
+  <h1>
+    Bonjour, {formatName(user)} !
+  </h1>
+);
+
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Bonjour, {formatName(user)} !</h1>;
+  }
+  return <h1>Bonjour, Belle Inconnue.</h1>;
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {element}
+        {getGreeting()}
+        {getGreeting(moi)}
+        
+        
       </header>
     </div>
   );
